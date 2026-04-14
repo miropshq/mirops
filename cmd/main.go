99b20ee70b20161ffb9da52f39f77c33ad385281
Enrique Cruz <enrique.cs095@unitec.edu>
2026-04-13 23:41:09 -0500

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -36,7 +36,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	miropsv1 "github.com/miropshq/mirops/api/v1"
-	"github.com/miropshq/mirops/internal/collector"
+	//"github.com/miropshq/mirops/internal/collector"
 	// +kubebuilder:scaffold:imports
 )
 
